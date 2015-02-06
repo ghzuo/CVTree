169dd465b62a99c1316b0aed88fef628adc40f9d
Guanghong Zuo <ghzuo@fudan.edu.cn>
2015-02-06 08:20:16 +0800

    update Node class to make more efficient

--- src/tree.h
+++ src/tree.h
@@ -14,8 +14,6 @@
 #include "global.h"
 using namespace std;
 
-typedef pair<string, string> str2str;
-
 struct Node{
     typedef vector<Node*> Children;
 
@@ -24,9 +22,6 @@ struct Node{
     double length;
     Node* parent;
     Children children;
-
-    size_t taxSize, nleaf, nxleaf, nUpLeaf; // nxleaf is the number of unclassfied leafs
-    bool unclassified, uploaded;
 
     Node();
     Node(size_t);
@@ -38,37 +33,13 @@ struct Node{
     void deleteChild(Node*);
     void getDescendants(vector<Node*>&);
     void getLeafs(vector<Node*>&);
-    bool isLeaf();
-    
-    void checkUnclassified();
-    void checkUploaded();
-    void getDefineLeafs(vector<Node*>&);
-    void getUndefineLeafs(vector<Node*>&);
-    void getUndefineNames(vector<string>&);
-    void chkLeafsName(vector<string>&, vector<string>&, bool);
-
-    string getStrainName();
-    void _getPrediction(string&);
-    void outPrediction(ostream&);
+    bool isLeaf();    
 
     Node* resetroot(const string&);
     Node* resetroot(Node*);
 
     void _outnwk(ostream&);
     void outnwk(ostream&);
-    void _innwk(istream&);
-    void innwk(istream&);
-
-    void _injson(istream&);
-    void _getStr(istream&, string&);
-    void _getKeyValue(string&, string&);
-    void injson(istream&);
-    void outjson(ostream&);
-    void outjsonAbbr(ostream&);
-
-    void renewId(const unordered_map<string,size_t>&);
-    void reinitTree();
-    void chgLeafName(const str2str&);
 };
 
 #endif
