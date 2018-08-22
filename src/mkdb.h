404d8204294e9fbce0e2438050448375de3126c5
Guanghong Zuo <ghzuo@fudan.edu.cn>
2018-08-22 22:26:04 +0800

    add mkdb command to product distance matrix but not tree

---
+++ src/mkdb.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2018  T-Life Research Center, Fudan University, Shanghai,
+ * China. See the accompanying Manual for the contributors and the way to cite
+ * this work. Comments and suggestions welcome. Please contact Dr. Guanghong Zuo
+ * <ghzuo@fudan.edu.cn>
+ *
+ * @Author: Dr. Guanghong Zuo
+ * @Date: 2017-03-08 20:30:43
+ * @Last Modified By: Dr. Guanghong Zuo
+ * @Last Modified Time: 2018-07-26 21:55:04
+ */
+
+#ifndef MKDB_H
+#define MKDB_H
+
+#include "cvmeth.h"
+#include "distmeth.h"
+#include "info.h"
+#include "neighborJoint.h"
+using namespace std;
+
+// read arguments
+struct Args {
+  string program, dmName;
+  string refdm;
+  vector<string> glist;
+  vector<size_t> klist;
+  bool netcdf;
+  float memorySize;
+
+  CVmeth *cmeth;
+  DistMeth *dmeth;
+
+  Args(int, char **);
+  void usage();
+};
+
+string nameWithK(const string &, size_t);
+void mkpath(const string &);
+
+#endif
